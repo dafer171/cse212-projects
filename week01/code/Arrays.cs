027f12171842f118f2fe40a71bc4b7e382628a35
David <davidp@ntestinc.com>
2024-09-23 16:08:51 +0200

    Rotating list ready

--- week01/code/Arrays.cs
+++ week01/code/Arrays.cs
@@ -42,7 +42,15 @@ public static class Arrays
         // Remember: Using comments in your program, write down your process for solving this problem
         // step by step before you write the code. The plan should be clear enough that it could
         // be implemented by another person.
-        for (var i = 0; i <= data.Count; i++)
-            data[i] = data[i + amount];
+
+        List<int> movedRight = new List<int>(new int[data.Count]);
+        // new list is necesary to hold the new values without losing the originals ones
+        for (var i = 0; i <= data.Count - 1; i++)
+            movedRight[i] = data[(i - amount + data.Count) % data.Count];
+        //rotating indexes
+        data.Clear();
+        data.AddRange(movedRight);
+        return;
     }
+
 }
