f33f47b0afe1d59ec9f061beaa44203e4f4eb88e
David <davidp@ntestinc.com>
2024-09-28 22:32:04 +0200

    First test PriorityQueue passed

--- week02/code/PriorityQueue_Tests.cs
+++ week02/code/PriorityQueue_Tests.cs
@@ -6,13 +6,23 @@ using Microsoft.VisualStudio.TestTools.UnitTesting;
 public class PriorityQueueTests
 {
     [TestMethod]
-    // Scenario: 
-    // Expected Result: 
-    // Defect(s) Found: 
+    // Scenario: Create a queue with items with data and priority 
+    // Expected Result: Repeat tests, 3; Tests, 4; Fix bugs, 5.
+    // Defect(s) Found: Nothing
     public void TestPriorityQueue_1()
     {
+        var repeating = new PriorityItem("Repeat Tests", 3);
+        var testing = new PriorityItem("Tests", 4);
+        var fixing = new PriorityItem("Fix bugs", 5);
+
+        var expectedString = "[Repeat Tests (Pri:3), Tests (Pri:4), Fix bugs (Pri:5)]";
+
         var priorityQueue = new PriorityQueue();
-        Assert.Fail("Implement the test case and then remove this.");
+        priorityQueue.Enqueue(repeating.Value, repeating.Priority);
+        priorityQueue.Enqueue(testing.Value, testing.Priority);
+        priorityQueue.Enqueue(fixing.Value, fixing.Priority);
+
+        Assert.AreEqual(expectedString, priorityQueue.ToString());
     }
 
     [TestMethod]
