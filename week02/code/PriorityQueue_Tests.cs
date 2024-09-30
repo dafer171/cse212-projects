6755dcdc8ee31905ac92715dfb99cd216ba0d7f9
David <davidp@ntestinc.com>
2024-09-30 21:58:19 +0200

    All tests passed

--- week02/code/PriorityQueue_Tests.cs
+++ week02/code/PriorityQueue_Tests.cs
@@ -27,7 +27,7 @@ public class PriorityQueueTests
 
     [TestMethod]
     // Scenario: Dequeue function shall remove the item with the highest priority and return its value
-    // Expected Result: Fix bugs, 5; Tests, 4; Repeat tests, 3.
+    // Expected Result: Fix bugs.
     // Defect(s) Found: Function does not remove the item with the highest priority 
     public void TestPriorityQueue_2()
     {
@@ -45,8 +45,48 @@ public class PriorityQueueTests
         var priorityItem = priorityQueue.Dequeue();
 
         Assert.AreEqual(expectedString, priorityItem);
-
     }
 
-    // Add more test cases as needed below.
+    [TestMethod]
+    // Scenario: If there are more than one item with the highest priority, then the item closest to the front of the queue will be removed and its value returned.
+    // Expected Result: Fix bugs.
+    // Defect(s) Found: Function retunr the last value
+    public void TestPriorityQueue_3()
+    {
+        var repeating = new PriorityItem("Repeat Tests", 3);
+        var testing = new PriorityItem("Tests", 4);
+        var fixing = new PriorityItem("Fix bugs", 5);
+        var documenting = new PriorityItem("Documenting", 5);
+
+        var expectedString = "Fix bugs";
+
+        var priorityQueue = new PriorityQueue();
+        priorityQueue.Enqueue(repeating.Value, repeating.Priority);
+        priorityQueue.Enqueue(testing.Value, testing.Priority);
+        priorityQueue.Enqueue(fixing.Value, fixing.Priority);
+        priorityQueue.Enqueue(documenting.Value, documenting.Priority);
+
+        var priorityItem = priorityQueue.Dequeue();
+
+        Assert.AreEqual(expectedString, priorityItem);
+
+    }
+    [TestMethod]
+    // Scenario: If the queue is empty, then an error exception shall be thrown.
+    // Expected Result: InvalidOperationException("The queue is empty.");
+    // Defect(s) Found: Nothing
+    public void TestPriorityQueue_4()
+    {
+        var priorityQueue = new PriorityQueue();
+
+        try
+        {
+            priorityQueue.Dequeue();
+            Assert.Fail("Exception should have been thrown.");
+        }
+        catch (InvalidOperationException e)
+        {
+            Assert.AreEqual("The queue is empty.", e.Message);
+        }
+    }
 }
